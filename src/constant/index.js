cb893fc67b95121308a5bc0801c065d61285f41b
Victor Teh Kian Mean <victor.teh@beans.com.my>
2023-01-27 15:50:47 +0800

    init

---
+++ src/constant/index.js
@@ -0,0 +1,4 @@
+export default Constants = {
+  ABOUT: '/about/',
+  HOME: '/home/'
+}
---
+++ src/views/AboutView/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'HomeView',
+  components: {
+  }
+}
+</script>
